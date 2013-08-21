ae9283a26df5af2b422f9fe3e10cb04509bfe96b
eggdropsoap <eggdropsoap@pipemaze.com>
2013-08-21 13:21:53 -0700

    Start with Forge boilerplate

---
+++ eggdropsoap/spreadinglilypads/client/ClientProxy.java
@@ -0,0 +1,12 @@
+package eggdropsoap.spreadinglilypads.client;
+
+import eggdropsoap.spreadinglilypads.CommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy {
+    
+    @Override
+    public void registerRenderers() {
+            // This is for rendering entities and so forth later on
+    }
+}
