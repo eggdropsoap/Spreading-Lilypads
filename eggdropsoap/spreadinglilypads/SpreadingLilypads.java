cb7597b3a2597868971c8c11b5f074931a64b543
eggdropsoap <eggdropsoap@pipemaze.com>
2013-08-21 21:38:10 -0700

    Remove testing name change and non-functional reflection code.

--- eggdropsoap/spreadinglilypads/SpreadingLilypads.java
+++ eggdropsoap/spreadinglilypads/SpreadingLilypads.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class SpreadingLilypads {
 
-	public Block spreadingLilyPad = (new BlockSpreadingLilyPad(500))
+	public Block spreadingLilyPad = (new BlockSpreadingLilyPad(500)) // FIXME: config for temp ID
 			.setHardness(0.0F).setStepSound(Block.soundGrassFootstep)
 			.setUnlocalizedName("waterlily")
 			.func_111022_d("waterlily")
@@ -47,46 +47,9 @@ public class SpreadingLilypads {
     //@Init       // used in 1.5.2
     public void load(FMLInitializationEvent event) {
             proxy.registerRenderers();
-          
-            LanguageRegistry.addName(spreadingLilyPad, "Spreading Lily Pad");
-            
-//            Block.blocksList[Block.waterlily.blockID] = null;
-//            spreadingLilyPad = (new BlockSpreadingLilyPad(5001)).setHardness(0.0F).setStepSound(Block.soundGrassFootstep).setUnlocalizedName("waterlily").func_111022_d("waterlily");
+                      
             Block.blocksList[Block.waterlily.blockID] = spreadingLilyPad;
             
-/*            try
-            {
-            	Class<?> block = Block.class;
-            	Field oldLilyPad = block.getDeclaredField("waterlily");		// field_71991_bz in 1.6.2
-            	Field blocksList = block.getDeclaredField("blocksList");	// field_71973_m in 1.6.2
-
-//            	blocksList.setAccessible(true);
-            	
-//            	try {
-					Utils.unfinalise(blocksList);
-//				} catch (Exception e) {
-					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-
-//            	Object ob = blocksList.get(null);
-            	
-//            	System.out.println("Reflected access: " + value.getUnlocalizedName());
-            	
-//            	try {
-//					Utils.unfinalise(oldLilyPad);
-//				} catch (Exception e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-            	
-            } catch (NoSuchFieldException e) {
-            	// TODO Auto-generated catch block
-            	e.printStackTrace();
-            } catch (SecurityException e) {
-            	// TODO Auto-generated catch block
-            	e.printStackTrace();
-            }*/
 
     }
    
